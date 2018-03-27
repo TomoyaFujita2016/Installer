60bef0c4ae8ab9cd84b40c2d7ac4d10691e872af
TomoyaFujita2016 <fujita.t.2016c@gmail.com>
2018-03-27 04:39:50 +0000

    added a line that install dpds

--- installHeadlessChrome.sh
+++ installHeadlessChrome.sh
@@ -1,4 +1,5 @@
 sudo apt-get install -y libappindicator1 fonts-liberation
+sudo apt-get install -fy
 cd /tmp
 curl -O https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
