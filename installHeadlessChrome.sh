3469822e78fc07c94856f61442bb8e0e24decec7
TomoyaFujita2016 <fujita.t.2016c@gmail.com>
2018-03-27 11:36:28 +0900

    added chrome

---
+++ installHeadlessChrome.sh
@@ -0,0 +1,4 @@
+sudo apt-get install -y libappindicator1 fonts-liberation
+cd /tmp
+curl -O https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
