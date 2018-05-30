14a8522b4fd905c2483db4e04469886e6cbbcd9c
Tomoya <fujita.t.2016c@gmail.com>
2018-05-30 11:53:24 +0900

    Update installPython3.sh

--- installPython3.sh
+++ installPython3.sh
@@ -1,11 +1,19 @@
+sudo apt-get update
 sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev
+sudo apt-get install -fy
 git clone https://github.com/yyuu/pyenv.git ~/.pyenv
-echo -e 'export PYENV_ROOT=$HOME/.pyenv\nexport PATH=$PYENV_ROOT/bin:$PATH\neval "$(pyenv init -)"' >> ~/.bashrc
+
+{ 
+echo 'export PYENV_ROOT=$HOME/.pyenv'
+echo 'export PATH=$PYENV_ROOT/bin:$PATH'
+echo 'eval "$(pyenv init -)"'
+} >> ~/.bashrc
+
 git clone https://github.com/yyuu/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
-echo -e 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
-echo eval "$(pyenv init -)" >> ~/.bashrc
+echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
 source ~/.bashrc
 sudo apt-get install -y zlib1g-dev
 pyenv install 3.6.4
-echo -e 'If you want to make virtual python environment, type "pyenv virtualenv 3.6.4 [virtual name]"'
-echo -e 'If you get error, make sure you type this command.\nsudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \nlibreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \nxz-utils tk-dev'
+echo -e '\033[1;39mIf you want to make virtual python environment, type "pyenv virtualenv 3.6.4 [virtual name]"\033[0;39m'
+echo -e '\033[0;33mIf you get error, make sure you type this command.\nsudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev\033[0;39m'
+echo -e '\033[0;33mAfter that, pyenv install 3.6.4\033[0;39m'
