84dafe2b3d570a1321fabba7daadec5043ec83ea
t-fujita24 <fujita.t.2016c@gmail.com>
2024-12-03 09:25:28 +0900

    Update installPython3.sh

--- installPython3.sh
+++ installPython3.sh
@@ -21,7 +21,7 @@ git clone https://github.com/yyuu/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-vi
 eval "$(pyenv virtualenv-init -)"
 echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bash_profile
 sudo apt-get install -y zlib1g-dev
-pyenv install 3.9.7
+pyenv install 3.11.4
 echo -e '\033[1;39mIf you want to make virtual python environment, type "pyenv virtualenv 3.9.7 [virtual name]"\033[0;39m'
 echo -e '\033[0;33mIf you get error, make sure you type this command.\nsudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev\033[0;39m'
 echo -e '\033[0;33mAfter that, pyenv install 3.6.4\033[0;39m'
