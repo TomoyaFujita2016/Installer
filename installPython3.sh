85d56bd75e81777624f014c11c86d3a712992453
t-fujita24 <fujita.t.2016c@gmail.com>
2024-12-30 01:34:00 +0900

    Update installPython3.sh

--- installPython3.sh
+++ installPython3.sh
@@ -1,5 +1,5 @@
 sudo apt-get update
-sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev
+sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev liblzma-dev
 sudo apt-get install -fy
 git clone https://github.com/yyuu/pyenv.git ~/.pyenv
 
